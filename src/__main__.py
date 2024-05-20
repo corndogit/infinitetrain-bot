acf9db573ec8b6f11e1fb18ce8bd066efb9d323c
Nathan <101812777+corndogit@users.noreply.github.com>
2024-05-20 19:40:15 +0100

    check if in current voice channel before moving and other refactoring

--- src/__main__.py
+++ src/__main__.py
@@ -6,11 +6,12 @@ from disnake.ext import commands
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 INFINITETRAIN_PATH = os.getenv('INFINITETRAIN_PATH')
-SHELL_COMMAND = f'cd {INFINITETRAIN_PATH} && ./infinitetrain tracks.yml - | ffmpeg -f flac -i - -filter:a "volume=0.3" -f flac -c:a flac -nostats -loglevel 0 -'
+FFMPEG_COMMAND = 'ffmpeg -f flac -i - -filter:a "volume=0.3" -f flac -c:a flac -nostats -loglevel 0 -'
+SHELL_COMMAND = f'cd {INFINITETRAIN_PATH} && ./infinitetrain tracks.yml - | {FFMPEG_COMMAND}'
 processes = {}
 
 bot = commands.Bot(
-    command_prefix=disnake.ext.commands.when_mentioned,
+    command_prefix=None,
     command_sync_flags=commands.CommandSyncFlags.default()
 )
 
@@ -21,37 +22,41 @@ async def on_ready():
 
 
 @bot.slash_command(description="TRAIN TRAIN I LOVE MY STATION")
-async def trainroll(inter):
-    connected = inter.author.voice
-    if not connected:
-        await inter.send("Error: you must be in a voice channel", ephemeral=True)
+async def trainroll(inter: disnake.ApplicationCommandInteraction):
+    voice_channel = inter.author.voice.channel
+    voice_client = disnake.utils.get(bot.voice_clients, guild=inter.guild)
+    if not voice_channel:
+        await inter.response.send_message("Error: you must be in a voice channel", ephemeral=True)
         return
+    if voice_client:
+        if voice_client.channel.id == voice_channel.id:
+            await inter.response.send_message("Already playing in this channel", ephemeral=True)
 
-    infinitetrain = subprocess.Popen(SHELL_COMMAND, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    processes[str(inter.guild)] = infinitetrain
+    infinitetrain = subprocess.Popen(SHELL_COMMAND,
+                                     shell=True,
+                                     stdin=subprocess.PIPE,
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.PIPE)
+    processes[inter.guild.id] = infinitetrain
     stream = FFmpegPCMAudio(infinitetrain.stdout, pipe=True)
 
-    voice = disnake.utils.get(bot.voice_clients, guild=inter.guild)
-    if voice and voice.is_connected:
-        await voice.move_to(connected.channel)
-    else:
-        voice = await connected.channel.connect()
+    voice = await voice_channel.connect()
     voice.play(stream)
-    await inter.send(f"Now playing in {connected.channel}")
+    await inter.response.send_message(f"Now playing in {voice_channel}")
 
 
 @bot.slash_command(description="Stop playing")
 async def trainstop(inter):
     voice = disnake.utils.get(bot.voice_clients, guild=inter.guild)
     if not voice:
-        await inter.send("Error: bot is not in a voice channel", ephemeral=True)
+        await inter.response.send_message("Error: bot is not in a voice channel", ephemeral=True)
         return
 
-    if processes[str(inter.guild)]:
-        processes[str(inter.guild)].kill()
+    if processes[inter.guild.id]:
+        processes[inter.guild.id].kill()
     voice.cleanup()
-    await voice.disconnect()
-    await inter.send("Left the voice channel")
+    await voice.disconnect(force=False)
+    await inter.response.send_message("Left the voice channel")
 
 
 bot.run(TOKEN)
